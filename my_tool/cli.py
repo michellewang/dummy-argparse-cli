264e4b9a57845f6a32a9e79c07e09c60691417fd
Michelle Wang <tomichellewang@gmail.com>
2026-06-12 19:26:02 -0400

    update parser

--- my_tool/cli.py
+++ my_tool/cli.py
@@ -6,17 +6,24 @@ DEFAULT_NAME = "world"
 def get_parser():
     parser = argparse.ArgumentParser(description="A dummy CLI tool for testing.")
     parser.add_argument("--name", type=str, help="Input file", default=DEFAULT_NAME)
+    parser.add_argument(
+        "--bye", action="store_true", help="Whether to say goodbye instead of hello."
+    )
     return parser
 
 
-def print_name(name: str = DEFAULT_NAME):
-    print(f"Hello, {name}!")
+def print_name(name: str = DEFAULT_NAME, bye: bool = False):
+    if not bye:
+        prefix = "Hello"
+    else:
+        prefix = "Goodbye"
+    print(f"{prefix}, {name}!")
 
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    print_name(args.name)
+    print_name(args.name, bye=args.bye)
 
 
 if __name__ == "__main__":
